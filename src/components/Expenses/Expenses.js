282b2dfbb041fb62e055ad00182b1b0f2aefc3c6
shibaftw2022 <doge>
2023-02-20 16:01:30 +0800

    lesson 66

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,40 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
+import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
+import React, {useState} from "react";
 
 const Expenses = (props) =>{
 
+    const [year,setYear] = useState('2020');
+
+    const passExpenseFilter = (expenseYear) =>{
+        setYear(expenseYear);
+    };
+
+    const filterExpenses = props.data.filter(expense =>{
+        return expense.date.getFullYear().toString() === year;
+    });
+
+    let expensesContent = <p>No expenses found.</p>
+
+    if(filterExpenses.length >0){
+        expensesContent = filterExpenses.map( (expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />);
+    };
+
+
     return(
+        <div>
+      
         <Card className="expenses">
-        <ExpenseItem title={props.data[0].title} amount={props.data[0].amount} date={props.data[0].date} />
-        <ExpenseItem title={props.data[1].title} amount={props.data[1].amount} date={props.data[1].date} />
-        <ExpenseItem title={props.data[2].title} amount={props.data[2].amount} date={props.data[2].date} />
-        <ExpenseItem title={props.data[3].title} amount={props.data[3].amount} date={props.data[3].date} />
+        <ExpenseFilter selected={year} passExpenseFilter = {passExpenseFilter}/>
+        {expensesContent}
+        {/* {filterExpenses.length === 0 && (expensesContent)}
+        {filterExpenses.length > 0 && (filterExpenses.map( (expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))} */}
+        {/* {filterExpenses.length === 0 ? (<p>No Expenses Found.</p>) : (filterExpenses.map( (expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))} */}
+
         </Card>
+        </div>
     )
 
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 
     // const [enteredTitle , setEnteredTitle] = useState('');
     // const [enteredAmount , setEnteredAmount] = useState('');
@@ -48,21 +48,41 @@ const ExpenseForm = () => {
         })
         console.log(userInput.enteredDate);
     };
+
+    const submitHandler = (event) =>{
+        event.preventDefault();
+        const expenseData = {
+          title : userInput.enteredTitle,
+          amount : userInput.enteredAmount,
+          date : new Date(userInput.enteredDate)
+        };
+        console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
+        setUserInput(() =>{
+          return { enteredTitle : '',
+          enteredAmount: '',
+          enteredDate: ''};
+      });
+
+    
+        
+    };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={userInput.enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler} />
+          <input type="number" min='0.01' step='0.01' value={userInput.enteredAmount} onChange={amountChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
+          <input type="date" min='2019-01-01' max='2022-12-31' value={userInput.enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
